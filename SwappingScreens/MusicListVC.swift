60e0f6f7e93f53426a7d89739c7f75facff92283
Header-Develop <kritawitbunket@gmail.com>
2017-01-22 12:50:28 +0700

    Add the button for back to main screen by code

--- SwappingScreens/MusicListVC.swift
+++ SwappingScreens/MusicListVC.swift
@@ -13,23 +13,13 @@ class MusicListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.blue
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
